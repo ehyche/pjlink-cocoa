5f100069adc2323d60658938d902b6490fbd56aa
Eric Hyche <ehyche@groupon.com>
2013-05-19 23:54:57 -0400

    Convert newlines to carriage returns. Resign text view first responder when Send button is pressed.

--- PJLinkCocoa/PJViewController.m
+++ PJLinkCocoa/PJViewController.m
@@ -31,19 +31,25 @@
 }
 
 - (IBAction)sendButtonTapped:(id)sender {
+    // Remove the keyboard
+    [self.requestTextView resignFirstResponder];
     // If the IP address or port changed, then we need to re-create the AFPJLinkClient
     if (_client == nil ||
         ![self.ipAddressTextField.text isEqualToString:_client.host] ||
         ![self.portTextField.text isEqualToString:[[NSNumber numberWithInteger:_client.port] stringValue]]) {
         // Create the new URL
-        NSURL* baseURL = [NSURL URLWithString:[NSString stringWithFormat:@"pjlink://%@:%@/", self.ipAddressTextField, self.portTextField]];
+        NSURL* baseURL = [NSURL URLWithString:[NSString stringWithFormat:@"pjlink://%@:%@/", self.ipAddressTextField.text, self.portTextField.text]];
         // Create a new AFPJLinkClient
         _client = [[AFPJLinkClient alloc] initWithBaseURL:baseURL];
     }
     // Start the activity indicator animating
     [self.responseActivityIndicator startAnimating];
+    // The UITextView by default puts in newlines (\n) instead of
+    // carriage returns (\r). So we replace all the occurrences
+    // of \n with \r.
+    NSString* requestText = [self.requestTextView.text stringByReplacingOccurrencesOfString:@"\n" withString:@"\r"];
     // Send the request
-    [_client makeRequestWithBody:self.requestTextView.text
+    [_client makeRequestWithBody:requestText
                          success:^(AFPJLinkRequestOperation* operation, NSString* responseBody, NSArray* parsedResponses) {
                              // Stop animating the activity indicator (this will also hide it)
                              [self.responseActivityIndicator stopAnimating];
